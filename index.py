bdeca8200e93c84031833e6bbf925d3973893ef7
Ariasaka <66897357+LYXOfficial@users.noreply.github.com>
2023-09-15 20:13:10 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,14 +1,5 @@
-import time,re,warnings
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.wait import WebDriverWait
-chrome_options = Options()
-chrome_options.add_argument("--mute-audio")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--headless')
+import time,re,warnings,requests
+from pyquery import PyQuery as pq
 warnings.filterwarnings("ignore")
 bs,ok,accounts,k=[],0,{},0
 with open("data.conf") as f:
@@ -19,56 +10,39 @@ with open("data.conf") as f:
             pass
 # ------------------------------------------------------------------------
 def run(acc,pwd):
-    global bs
-    browser.get("https://qoj.fzoi.top/login")
-    browser.refresh()
+    global bs,st
     print("Logining")
-    if acc in browser.execute_script("return document.body.innerHTML"):
-        browser.execute_script("""document.querySelectorAll(".nav-link")[document.querySelectorAll(".nav-link").length-1].click()""")
-    rp=20
-    while(1):
-        if not rp:
-            print("Time out")
-            raise Exception
-        if not browser.execute_script("""return document.querySelector("[name='username']")==null"""):
-            break
-        time.sleep(0.5)
-        rp-=1
-    browser.execute_script("""document.querySelector("[name='username']").value="%s";document.querySelector("[name='password']").value="%s";document.querySelector("#button-submit").click()"""%(acc,pwd))
-    print("Waiting For Login")
-    rp=20
-    while(1):
-        if not rp:
-            print("Time out")
-            raise Exception
-        try:
-            if "签到" in browser.execute_script("""return document.querySelectorAll(".card-body")[7].innerText"""):
-                break
-        except: pass
-        try:
-            if "错误" in browser.execute_script("""return document.body.innerText"""):
-                print("%s: Invaild Password\n------------------------------------------"%acc)
-                return
-        except: pass
-        time.sleep(0.5)
-        rp-=1
-    try:
-        b=browser.execute_script("""return document.querySelectorAll(".card-body")[7].innerText""")
-        if "你已经连续签到" in b:
-            print("%s Already Signed\n%s\n------------------------------------------"%(acc,b))
-            bs.append(b)
-            return
-    except:
-        pass
-    print("Try to Click")
-    browser.execute_script("""document.querySelector(\'.btn.btn-primary.btn-lg\').click()""")
-    time.sleep(2)
-    b=browser.execute_script("""return document.querySelectorAll(".card-body")[7].innerText""")
-    print("%s OK\n%s\n------------------------------------------"%(acc,b))
+    headers={"content-type":"application/x-www-form-urlencoded; charset=UTF-8","user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82"}
+    data={"_token": "Lep0EHXPsInDznO201QMeY14AX8c9VQtXY76AkvmGqJD7mxVk7TAI1bdVj5N",
+    "username": acc,
+    "password": pwd,"login":"","ip":""}
+    re=requests.post("https://qoj.fzoi.top/login",headers=headers,data=data)
+    if(re.text=="账号或密码错误"):
+        print("%s: Invaild password or username."%acc)
+        return
+    elif re.text=="banned":
+        print("%s: Banned."%acc)
+        return
+    elif re.text!="ok":
+        print("%s: Unknown Error"%acc)
+        return
+    print("Try to sign")
+    headers["cookie"]=re.headers["Set-Cookie"]
+    req=requests.get("https://qoj.fzoi.top/punch",headers=headers)
+    if req.status_code==200:
+        doc=pq(requests.get("https://qoj.fzoi.top",headers=headers).text)
+        b=list(doc(".card-body").items())[7].text()
+        print("%s OK, Time:%.2fs\n%s\n------------------------------------------"%(acc,time.time()-st,b))
+    elif req.status_code==403:
+        doc=pq(requests.get("https://qoj.fzoi.top",headers=headers).text)
+        b=list(doc(".card-body").items())[7].text()
+        print("%s Already Signed, Time: %.2fs\n%s\n------------------------------------------"%(acc,time.time()-st,b))
+    else: raise Exception("Unknown Error")
     bs.append(b)
-browser = webdriver.Chrome(options=chrome_options,executable_path='./chromedriver')
-print("FZQOJ Auto Sign System By Ariasaka v3.2.0\n------------------------------------------\nStart Processing")
+print("FZQOJ Auto Sign System By Ariasaka v4.0.0\n------------------------------------------\nStart Processing")
+stt=time.time()
 for acc,pwd in accounts.items():
+    st=time.time()
     try:
         print("Task #%d\nRunning %s"%(k,acc))
         run(acc,pwd)
@@ -82,12 +56,10 @@ for acc,pwd in accounts.items():
                 ok+=1
             except:
                 print("%s #%d Still Failed\n%s\n------------------------------------------"%(acc,i,e))
-            browser.delete_all_cookies()
-    browser.delete_all_cookies()
     k+=1
 day,rp=0,0
 for string in bs:
     r=re.findall("你已经连续签到 (.*?) 天，今日获得 (.*?) RP值和 1 硬币",string)
     day+=int(r[0][0])
     rp+=int(r[0][1])
-print("Arrage Days: %.2f && Today RP: %.2f"%(day/ok,rp/ok))
+print("Arrage Days: %.2f && Today RP: %.2f && Total Time: %.2fs"%(day/ok,rp/ok,time.time()-stt))
